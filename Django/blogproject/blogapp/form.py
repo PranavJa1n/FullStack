775d980b6de960cf5831d1e2728622fada6ea253
Pranav Jain <pranav.avlok@gmail.com>
2026-04-10 08:52:03 +0530

    many things

---
+++ Django/blogproject/blogapp/form.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'image']
+        
